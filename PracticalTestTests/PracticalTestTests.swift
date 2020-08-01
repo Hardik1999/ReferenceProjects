107287f60ca02535b2850f418eb2d69064b14905
hardik <hardikdudhrejiya07@gmail.com>
2020-08-01 13:24:37 +0530

    Initial Commit

---
+++ PracticalTestTests/PracticalTestTests.swift
@@ -0,0 +1,34 @@
+//
+//  PracticalTestTests.swift
+//  PracticalTestTests
+//
+//  Created by Hardik on 8/1/20.
+//  Copyright © 2020 macbook. All rights reserved.
+//
+
+import XCTest
+@testable import PracticalTest
+
+class PracticalTestTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
